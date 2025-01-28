3884f142fc096440f2f76aa92fff875b1de5a832
Mateus Lima <limamateus@live.com>
2025-01-27 22:16:04 -0300

    5 - Camada De Controllers : responsáveis por gerenciar a lógica de negócios da aplicação.

---
+++ lib/controllers/api_controller.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:hyrule/data/api/data_api.dart';
+import 'package:hyrule/domain/business/api_workflow.dart';
+import 'package:hyrule/domain/models/entrey.dart';
+
+class ApiController implements ApiWorkflow{
+final DataApi dataApi = DataApi();
+
+  @override
+  Future<List<Entry>> getEntriesByCategory({required String category}) {
+
+    return dataApi.getEntriesByCategory(category: category);
+  }
+
+}
